f604526f7c4e30b213da59f6c6b79b17c370bc9b
CleoSe <owen.shimer@gmail.com>
2026-01-28 16:36:02 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
-const path = require('path');
+const path = require('path');   
 const port = process.env.PORT || 8080;
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'home.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
