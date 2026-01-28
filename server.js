b7ec585d7f3821532c2dd6c0f68fa8c95bcb86d9
CleoSe <owen.shimer@gmail.com>
2026-01-28 16:52:04 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
-const express = require('express');
-const app = express();
-const path = require('path');   
-const port = process.env.PORT || 8080;
+const express = require("express");
+const path = require("path");
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+const app = express();
+
+// Render provides the PORT via environment variable
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Serve the HTML file
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
