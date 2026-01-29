079c5fab496cdc6ded50db02572ec21a3a0e6d15
CleoSe <owen.shimer@gmail.com>
2026-01-29 11:57:20 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const path = require("path");
 
 const app = express();
+dotenv.config();
 
 // Render provides the PORT via environment variable
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, "public")));
@@ -13,6 +15,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/about", (req, res) =>{
+    res.sendFile(path.join(__dirname, "public", "about.html"));
+})
+
+app.get("/project", (req, res) =>{
+    res.sendFile(path.join(__dirname, "public", "project.html"));
+})
+
 
 // Start the server
 app.listen(PORT, () => {
